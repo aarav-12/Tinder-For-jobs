bd291d9d574e38c6c026caa8df22990a505c39b1
Aarav Mithrani <aarav.mi33@gmail.com>
2025-12-26 00:41:11 +0530

    day 2 tried the ui but got stuck in version errors

---
+++ tinder-for-jobs/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-center bg-gray-500">
+      <h1 className="text-4xl font-bold text-black">
+        Tinder for Jobs 🚀
+      </h1>
+    </main>
+  );
+}
